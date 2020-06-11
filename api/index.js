3629a21e5777cf68115663068c2c167ce556e4fc
ignatif <ignatif@gmail.com>
2020-06-12 01:14:27 +0300

    post /page

--- api/index.js
+++ api/index.js
@@ -15,8 +15,22 @@ app.use(bodyParser.json());
 
 app.get("/pages", getPages);
 app.get("/pages/:id", getPage);
+
 app.post("/pages", createPage);
 app.post("/page-create", createPage);
+
+app.post(`/page`, async (req, res) => {
+  const { title, content, authorEmail } = req.body;
+  const result = await prisma.post.create({
+    data: {
+      title,
+      content,
+      published: false,
+      author: { connect: { email: authorEmail } },
+    },
+  });
+  res.json(result);
+});
 
 const server = app.listen(3000, () =>
   console.log("🚀 Server ready at: http://localhost:3000")
