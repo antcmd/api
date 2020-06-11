a4a81dbe5ac32ad1f62553765e1c09ed0967f842
Ubuntu <ubuntu@ip-172-31-63-201.ec2.internal>
2020-06-11 22:12:41 +0000

    update

--- api/index.js
+++ api/index.js
@@ -1,33 +1,23 @@
-export default async (req, res) => {
-  res.status(200).json({ up: true });
-};
+const express = require("express");
+const bodyParser = require("body-parser");
+const cors = require("cors");
+const { PrismaClient } = require("@prisma/client");
 
-// const express = require("express");
-// const bodyParser = require("body-parser");
-// const cors = require("cors");
-// const { PrismaClient } = require("@prisma/client");
+const getPages = require("./getPages");
+const getPage = require("./getPage");
+const createPage = require("./createPage");
 
-// const getPages = require("./getPages");
-// const getPage = require("./getPage");
-// const createPage = require("./createPage");
+const prisma = new PrismaClient();
+const app = express();
 
-// const prisma = new PrismaClient();
-// const app = express();
+app.use(cors({ origin: ["http://antcmd.now.sh/", "http://antglobe.now.sh/"] }));
+app.use(bodyParser.json());
 
-// var corsOptions = {
-//   origin: "http://example.com",
-//   optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
-// };
+app.get("/pages", getPages);
+app.get("/pages/:id", getPage);
+app.post("/pages", createPage);
+app.post("/page-create", createPage);
 
-// app.use(cors({ origin: ["http://antcmd.now.sh/", "http://antglobe.now.sh/"] }));
-
-// app.use(bodyParser.json());
-
-// app.get("/pages", getPages);
-// app.get("/pages/:id", getPage);
-// app.post("/pages", createPage);
-// app.get("/page-create", createPage);
-
-// const server = app.listen(3000, () =>
-//   console.log("🚀 Server ready at: http://localhost:3000")
-// );
+const server = app.listen(3000, () =>
+  console.log("🚀 Server ready at: http://localhost:3000")
+);
